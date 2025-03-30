2bb8b642243cf029b8b86d54b0b4db46c42facc4
Ejikedickson <engromeke@gmail.com>
2025-03-30 20:57:03 +0100

    table recreated with constraints

--- sql_query.sql
+++ sql_query.sql
@@ -7,5 +7,7 @@ CREATE TABLE exchange_rate_ngn(
 	ex_id SERIAL PRIMARY KEY,
 	curr_code VARCHAR(10),
 	rate NUMERIC,
-	ex_date DATE
+	ex_date DATE,
+	FOREIGN KEY (curr_code)
+	REFERENCES currency (curr_code)
 );
