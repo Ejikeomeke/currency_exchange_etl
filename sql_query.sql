4fa9f22802e04247a803905dfcd6486735d55130
Ejikedickson <engromeke@gmail.com>
2025-03-30 20:40:11 +0100

    first codes commit

---
+++ sql_query.sql
@@ -0,0 +1,11 @@
+CREATE TABLE currency(
+	curr_code VARCHAR(10) PRIMARY KEY,
+	curr_denom VARCHAR(100)
+);
+	
+CREATE TABLE exchange_rate_ngn(
+	ex_id SERIAL PRIMARY KEY,
+	curr_code VARCHAR(10),
+	rate NUMERIC,
+	ex_date DATE
+);
